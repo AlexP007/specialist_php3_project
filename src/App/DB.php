79b8f70eac11763ebaca533f0df23ca8f16ad434
AlexP <alex.p.panteleev@gmail.com>
2020-10-17 21:21:53 +0300

    [i] blog

--- src/App/DB.php
+++ src/App/DB.php
@@ -3,6 +3,7 @@
 
 namespace App;
 
+
 class DB
 {
     private static $instance;
---
+++ src/Model/Blog.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace Model;
+
+
+class Blog
+{
+    public function save(): bool
+    {
+
+    }
+
+    public function getAll(): array
+    {
+
+    }
+}
